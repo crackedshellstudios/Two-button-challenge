eb1e12f4ffd93af8ed0e2ee9161d86d905b84c52
crackedshellstudios <crackedshellstudios@gmail.com>
2023-05-24 23:46:31 -0500

    Initial Commit

---
+++ Two button challenge/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Two button challenge
+//
+//  Created by Lucas Lowder on 5/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("App loaded successfully")
+    }
+
+
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        print("showMessagePressed successful")
+        messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.systemTeal
+    }
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        print("showAnotherMessagePressed successful")
+        messageLabel.text = "You are great!"
+        messageLabel.textColor = UIColor.systemOrange
+    }
+}
+
